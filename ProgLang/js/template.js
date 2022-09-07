db90d1ae811379335b8180a6fe5ce2cada0df47c
Xeltalliv <vadim@vvv.kiev.ua>
2022-09-07 12:57:04 +0300

    Add Sprite2

--- ProgLang/js/template.js
+++ ProgLang/js/template.js
@@ -53,6 +53,36 @@ let template = {
 		"direction":90,
 		"draggable":false,
 		"rotationStyle":"all around"
+	},{
+		"isStage":false,
+		"name":"Sprite2",
+		"variables":{},
+		"lists":{},
+		"broadcasts":{},
+		"blocks":{},
+		"comments":{},
+		"currentCostume":0,
+		"costumes":[{
+			"name":"costume1",
+			"bitmapResolution":1,
+			"dataFormat":"svg",
+			"assetId":"14e46ec3e2ba471c2adfe8f119052307",
+			"md5ext":"14e46ec3e2ba471c2adfe8f119052307.svg",
+/*			"assetId":"592bae6f8bb9c8d88401b54ac431f7b6",
+			"md5ext":"592bae6f8bb9c8d88401b54ac431f7b6.svg",*/
+			"rotationCenterX":44,
+			"rotationCenterY":44
+		}],
+		"sounds":[],
+		"volume":100,
+		"layerOrder":1,
+		"visible":false,
+		"x":0,
+		"y":0,
+		"size":100,
+		"direction":90,
+		"draggable":false,
+		"rotationStyle":"all around"
 	}],
 	"monitors":[],
 	"extensions":[],
